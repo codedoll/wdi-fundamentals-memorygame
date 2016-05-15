f3c0c975e26458d3f93723da7a8dfc2b9e618f5e
Shirlyn Dee <lyn@Shirlyns-MacBook-Pro.local>
2016-05-15 16:22:34 -0400

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,21 @@
-console.log("JS file is connected to HTML! Woo!")
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+
+if (cardOne===cardTwo)
+	alert("You found a match.")
+else if (cardThree===cardFour)
+	alert("You found a match!")
+else if (cardOne===cardThree)
+	alert("Sorry no match.")
+else if (cardTwo===cardThree)
+	alert("Sorry no match.")
+else if (cardOne===cardFour)
+	alert("Sorry no match.")
+else if (cardTwo===cardFour)
+	alert("Sorry no match.")
+else
+	alert("No logic yet")
+
